72c1b0e2a63e0b4095454235b358a020ba80b808
Renan Liberato <renan.libsantana@gmail.com>
2017-05-09 17:47:26 -0300

    AuthController test implementation

---
+++ module/Application/test/Controller/AuthControllerTest.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: renan
+ * Date: 9/5/2017
+ * Time: 4:58 PM
+ */
+
+namespace ApplicationTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AuthControllerTest extends AbstractHttpControllerTestCase
+{
+    const URL = "/auth";
+
+//    /**
+//     * @var Client
+//     */
+//    private $client;
+//
+//    /**
+//     * @var Request
+//     */
+//    private $request;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            include(__DIR__.'/../../../../config/application.config.php')
+        );
+
+        parent::setUp();
+
+//        $this->client = new Client();
+//        $this->request = new Request();
+//        $this->request->setUri(self::BASE_URL."/auth");
+    }
+
+    public function testIfGetIsNotAllowed()
+    {
+        $this->getRequest()->setMethod("GET");
+        $this->dispatch(self::URL);
+        $response = $this->getResponse();
+
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testIfEmptyBodyIsNotAllowed()
+    {
+        $this->getRequest()->setMethod("POST");
+        $this->dispatch(self::URL);
+        $response = $this->getResponse();
+
+        $this->assertEquals(400, $response->getStatusCode());
+    }
+
+    public function testIfNonExistentUserIsNotAllowed()
+    {
+        $this->getRequest()->setMethod("POST")->setContent(json_encode([
+            "username" => "nonexistent",
+            "password" => "nonexistent"
+        ]));
+
+        $this->dispatch(self::URL);
+        $response = $this->getResponse();
+
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    public function testIfWrongPasswordIsNotAllowed()
+    {
+        $this->getRequest()->setMethod("POST")->setContent(json_encode([
+            "username" => "jose",
+            "password" => "nonexistent"
+        ]));
+
+        $this->dispatch(self::URL);
+        $response = $this->getResponse();
+
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+
+    public function testIfCorrectCredentialsIsAllowed()
+    {
+        $this->getRequest()->setMethod("POST")->setContent(json_encode([
+            "username" => "jose",
+            "password" => "jose"
+        ]));
+
+        $this->dispatch(self::URL);
+        $response = $this->getResponse();
+
+        $jsonBody = $response->getContent();
+
+        $objectBody = json_decode($jsonBody);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertObjectHasAttribute("token", $objectBody);
+        $this->assertEquals(3, count(explode('.', $objectBody->token)));
+    }
+}
