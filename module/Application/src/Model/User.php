7ebf502be3667b4aa8399ae8ae016338b6195a22
Renan Liberato <renan.libsantana@gmail.com>
2017-05-09 17:38:55 -0300

    Implemented User model

---
+++ module/Application/src/Model/User.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: renan
+ * Date: 9/5/2017
+ * Time: 4:44 PM
+ */
+
+namespace Application\Model;
+
+
+class User
+{
+
+    private $id;
+
+    private $username;
+
+    private $password;
+
+    /**
+     * User constructor.
+     * @param $id
+     * @param $username
+     * @param $password
+     */
+    public function __construct($id = null, $username = null, $password = null)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+}
